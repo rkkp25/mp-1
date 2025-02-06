792e41c1982685d5005f5bf9e06ab3b5b8333f8c
rkkp25 <rkkp@bu.edu>
2025-02-06 02:18:13 -0500

    adding all files

---
+++ script.js
@@ -0,0 +1,83 @@
+function doAdd() {
+    let input1 = Number(document.getElementById("input1").value);
+    let input2 = Number(document.getElementById("input2").value);
+    let output = String(input1 + input2);
+
+    if (Number(output) < 0) {
+        document.getElementById("output").style.color = "red";
+    }
+    else {
+        document.getElementById("output").style.color = "black";
+    }
+
+    document.getElementById("output").innerHTML = output;
+}
+
+function doSub() {
+    let input1 = Number(document.getElementById("input1").value);
+    let input2 = Number(document.getElementById("input2").value);
+    let output = String(input1 - input2);
+
+    if (Number(output) < 0) {
+        document.getElementById("output").style.color = "red";
+    }
+    else {
+        document.getElementById("output").style.color = "black";
+    }
+
+    document.getElementById("output").innerHTML = String(input1 - input2);
+}
+
+function doMulti() {
+    let input1 = Number(document.getElementById("input1").value);
+    let input2 = Number(document.getElementById("input2").value);
+    let output = String(input1 * input2);
+
+    if (Number(output) < 0) {
+        document.getElementById("output").style.color = "red";
+    }
+    else {
+        document.getElementById("output").style.color = "black";
+    }
+
+    document.getElementById("output").innerHTML = String(input1 * input2);
+}
+
+function doDiv() {
+    let input1 = Number(document.getElementById("input1").value);
+    let input2 = Number(document.getElementById("input2").value);
+    let output = String(input1 / input2);
+
+    if (Number(output) < 0) {
+        document.getElementById("output").style.color = "red";
+    }
+    else {
+        document.getElementById("output").style.color = "black";
+    }
+
+    document.getElementById("output").innerHTML = String(input1 / input2);
+}
+
+function doPow() {
+    let input1 = Number(document.getElementById("input1").value);
+    let input2 = Number(document.getElementById("input2").value);
+
+    let output = 1;
+    for(let i = 0; i < input2; i++) {
+        output = output * input1;
+    }
+
+    if (Number(output) < 0) {
+        document.getElementById("output").style.color = "red";
+    }
+    else {
+        document.getElementById("output").style.color = "black";
+    }
+
+    document.getElementById("output").innerHTML = String(output);
+}
+
+function doClear() {
+    document.getElementById("output").innerHTML = "";
+}
+
